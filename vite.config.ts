f1573ff88e659778e982a7a759d09a2f444dc58b
wajihmathlouthi21-commits <wajihmathlouthi21@gmail.com>
2026-07-20 16:33:08 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     allowedHosts: [
-      'support-hub-4x0t.onrender.com'
-    ]
+      '://onrender.com'
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // تأكد أن هذا هو نفس المنفذ (PORT) المكتوب في ملف server.ts
+        changeOrigin: true,
+        secure: false,
+      }
+    }
   }
 });
