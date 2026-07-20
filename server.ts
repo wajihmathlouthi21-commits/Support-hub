ae4b2e191b8af1fbb517ed1ee943da74245059e3
wajihmathlouthi21-commits <wajihmathlouthi21@gmail.com>
2026-07-20 16:34:26 +0100

    Update server.ts

--- server.ts
+++ server.ts
@@ -26,7 +26,11 @@ import {
 } from './src/db.js';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 
 // Enable JSON parse middleware
 app.use(express.json({ limit: '100mb' }));
